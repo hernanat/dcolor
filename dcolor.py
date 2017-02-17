8dadd2c05f81ddda57f38c543405637eef262dd7
Anthony Hernandez <hernanat@bu.edu>
2017-02-17 13:18:56 -0500

    add shebang and docstrings

--- dcolor.py
+++ dcolor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as mcolors
@@ -15,36 +17,38 @@ class DColor:
         self.makeDomain()
 
     def makeDomain(self):
+        """Create the domains for Real (x) and Imaginary (y) values respectively"""
         x = np.linspace(self._xmin, self._xmax, self._samples)
         y = np.linspace(self._ymin, self._ymax, self._samples)
         self.xx, self.yy=np.meshgrid(x,y)
 
     def makeColorModel(self, zz):
+        """Create the HSV color model for the function domain that will be plotted"""
         H = self.normalize(np.mod(np.angle(zz),2*np.pi)) #Hue determined by arg(z)
         r = np.log2(np.add(1.0,np.abs(zz)))
         S = np.divide(np.add(1.0,np.abs(np.sin(np.multiply(2.0*np.pi,r)))),2.0)
-        #S = np.ones_like(H)
         V = np.divide(np.add(1.0,np.abs(np.cos(np.multiply(2*np.pi,r)))),2.0)
-        #V = np.ones_like(H)
-
-        #V[np.abs(np.subtract(V,0.01))<0.01]+=0.01
 
         return H,S,V
 
     def normalize(self, arr):
+        """Used for normalizing data in array based on min/max values"""
         arrMin = np.min(arr)
         arrMax = np.max(arr)
         arr = np.subtract(arr,arrMin)
         return np.divide(arr, arrMax-arrMin)
 
     def plot(self, f, xdim=10, ydim=8, plt_dpi=100):
-        x = np.linspace(self._xmin, self._xmax, self._xmax-self._xmin)
-        y = np.linspace(self._ymin, self._ymax, self._ymax-self._ymin)
-
+        """Plot a complex-valued function
+            Arguments:
+            f -- a (preferably) lambda-function defining a complex-valued function
+            Keyword Arguments:
+            xdim -- x dimensions
+            ydim -- y dimensions
+            plt_dpi -- density of pixels per inch
+        """
         zz=f(self.z(self.xx,self.yy))
         H,S,V = self.makeColorModel(zz)
-
-
         rgb = hsv_to_rgb(np.dstack((H,S,V)))
 
         fig = plt.figure(figsize=(xdim, ydim), dpi=plt_dpi)
@@ -55,12 +59,15 @@ class DColor:
         plt.show()
 
     def z(self, x, y):
+        """return complex number x+iy
+            If inputs are arrays, then it returns an array with corresponding x_j+iy_j values
+        """
         return x+1j*y
 
 dc = DColor(xmin=-10, xmax=10, ymin=-10, ymax=10, samples=4000)
 #dc.plot(lambda z : (z**5+1j*z**4+z**3+1j*z**2+z+1j+1)/np.sin(z))
-#dc.plot(lambda z : z)
+dc.plot(lambda z : z**3+2j*z**2+2j+z)
 #dc.plot(lambda z : (1/(1-1j*z))-(1+1j*z))
 #dc.plot(lambda z : z**3)
-dc.plot(lambda z : ((z+1-2j)*(z+2+2j)*((z-2)**2))/(z**3))
-#dc.plot(lambda z : np.sin(1/z))
+#dc.plot(lambda z : ((z+1-2j)*(z+2+2j)*((z-2)**2))/(z**3))
+#dc.plot(lambda z : np.sin(z))
