2033a8771601adaff2a8105ca6049ee82932ae1d
Arthur-Milchior <arthur@milchior.fr>
2019-04-04 07:58:05 +0200

    Adding a save function.
    Also separating test, so that dconfig can be imported without
    triggering it

--- dcolor.py
+++ dcolor.py
@@ -38,7 +38,7 @@ class DColor:
         arr = arr - arrMin
         return arr / (arrMax - arrMin)
 
-    def plot(self, f, xdim=10, ydim=8, plt_dpi=100):
+    def getPlot(self, f, xdim=10, ydim=8, plt_dpi=100):
         """Plot a complex-valued function
             Arguments:
             f -- a (preferably) lambda-function defining a complex-valued function
@@ -58,6 +58,13 @@ class DColor:
         plt.gca().get_yaxis().set_visible(False)
         plt.show()
 
+    def showPlot(self, f, xdim=10, ydim=8, plt_dpi=100):
+        plt = self.getPlot(f, xdim=10, ydim=8, plt_dpi=100)
+
+    def savePlot(self, f, fname, xdim=10, ydim=8, plt_dpi=100):
+        plt = self.getPlot(f, xdim=10, ydim=8, plt_dpi=100)
+        plt.savefig(fname, bbox_inches = tight)
+
     def z(self, x, y):
         """return complex number x+iy
             If inputs are arrays, then it returns an array with corresponding x_j+iy_j values
@@ -65,9 +72,3 @@ class DColor:
         return x+1j*y
 
 dc = DColor(xmin=-10, xmax=10, ymin=-10, ymax=10, samples=4000)
-#dc.plot(lambda z : (z**5+1j*z**4+z**3+1j*z**2+z+1j+1)/np.sin(z))
-dc.plot(lambda z : z**3+2j*z**2+2j+z)
-#dc.plot(lambda z : (1/(1-1j*z))-(1+1j*z))
-#dc.plot(lambda z : z**3)
-#dc.plot(lambda z : ((z+1-2j)*(z+2+2j)*((z-2)**2))/(z**3))
-#dc.plot(lambda z : np.sin(z))
